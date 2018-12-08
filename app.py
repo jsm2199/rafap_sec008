8709f01522ce5050ab4b40df3c1017e9d985e699
jsm2199 <jsm2199@gmail.com>
2018-12-07 16:05:24 -0800

    Start Lec106

--- app.py
+++ app.py
@@ -16,11 +16,6 @@ app.secret_key = 'secret_key'
 api = Api(app)
 
 
-@app.before_first_request
-def create_table():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
